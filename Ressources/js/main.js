f63c522b33c43cc8d43eb1e97eee603f7c3f857e
Jordan Bertieaux <jordan.bertieaux@std.heh.be>
2019-05-25 15:02:41 +0200

    Responsive Languages

--- Ressources/js/main.js
+++ Ressources/js/main.js
@@ -45,10 +45,12 @@ $(document).ready(function () {
     $('.contact100-btn-hide').on('click', function() { $('.wrap-contact100').fadeOut(400); });
     $('.contact100-btn-show').on('click', function() { $('.wrap-contact100').fadeIn(400); });
 
-    map();
-
     hiddecontent();
-    showSlides(slideIndex);
+
+    /* Scroll Events */
+    showSlides(1);
+    counter();
+    map();
 });
 
 /* #####################
